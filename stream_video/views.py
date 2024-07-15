9cf65dd224a2c4eb64f3246e3ef63b71f0f0beed
BarunBlog <bhattacharjeebarun25@gmail.com>
2024-07-15 21:06:26 +0600

    Removed unused view

--- stream_video/views.py
+++ stream_video/views.py
@@ -65,57 +65,6 @@ class GetVideos(generics.ListAPIView):
     serializer_class = GetVideosSerializer
 
 
-class StreamVideo(APIView):
-    
-    def get(self, request):
-        video_path = 'stream_video/videos/nature_video.mp4'
-        file_path = os.path.join(settings.STATIC_ROOT, video_path)
-
-        if not os.path.exists(file_path):
-            return HttpResponse('File not found', status=404)
-        
-        file_size = os.path.getsize(file_path)
-        range_header = request.headers.get('Range')
-        content_type = 'video/mp4'
-
-        if range_header:
-            start_byte, end_byte = 0, None
-
-            # Check if the range_header is in the format
-            # bytes=100-200
-            has_match = re.match(r'bytes=(\d+)-(\d*)', range_header)
-
-            if has_match:
-                start_byte, end_byte = has_match.groups()
-            
-            start_byte = int(start_byte)
-
-            if end_byte:
-                end_byte = int(end_byte)
-            else:
-                end_byte = file_size - 1
-
-            print(f"start_byte: {start_byte}, end_byte: {end_byte}", flush=True)
-
-            # Get the inclusive length of the byte range
-            length = end_byte - start_byte + 1
-
-            # Open the file in binary read mode and return the stream
-            with open(file_path, 'rb') as file_stream:
-                file_stream.seek(start_byte)  # Move pointer to the start_byte position
-                data = file_stream.read(length)  # Reads length bytes from start_byte
-            
-            response = HttpResponse(data, status=206, content_type=content_type)
-            response['Content-Length'] = str(length)
-            response['Content-Range'] = f'bytes {start_byte}-{end_byte}/{file_size}'
-        else:
-            response = StreamingHttpResponse(open(file_path, 'rb'), content_type=content_type)
-            response['Content-Length'] = str(file_size)
-
-        response['Accept-Ranges'] = 'bytes'
-        return response
-
-
 class ServeMPDFile(APIView):
     def get(self, request, *args, **kwargs):
         mpd_file_path = os.path.join(settings.STATIC_ROOT, 'stream_video', 'segments', 'manifest.mpd')
