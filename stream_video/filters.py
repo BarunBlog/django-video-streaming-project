d4866749c6517d07d2e62986e8a7bd10b3f23691
BarunBlog <bhattacharjeebarun25@gmail.com>
2024-08-10 19:37:18 +0600

    Updated the get videos filter

--- stream_video/filters.py
+++ stream_video/filters.py
@@ -1,10 +1,12 @@
-from django_filters import rest_framework as filters
+import django_filters as filters
 from .models import Video
 
 
 class VideoFilter(filters.FilterSet):
+    title = filters.CharFilter(field_name='title', lookup_expr='icontains')
     category = filters.CharFilter(field_name='category', lookup_expr='iexact')
+    created_at = filters.DateFilter(field_name='upload_date', lookup_expr='exact')
 
     class Meta:
         model = Video
-        fields = ['category']
+        fields = ['title', 'category', 'created_at']
