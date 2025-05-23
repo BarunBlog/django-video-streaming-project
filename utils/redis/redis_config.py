26b2f727348d5b6fa0f7616c850738c1aeaa123b
BarunBlog <bhattacharjeebarun25@gmail.com>
2025-05-23 13:46:13 +0600

    Updated the redis cluster config and set to run only in production environment

--- utils/redis/redis_config.py
+++ utils/redis/redis_config.py
@@ -1,28 +1,32 @@
 import redis
 from django.conf import settings
 
-# # Redis client configuration
-# redis_client = redis.StrictRedis(
-#     host=settings.REDIS_HOST,
-#     port=settings.REDIS_PORT,
-#     db=settings.REDIS_DB,
-#     decode_responses=True  # Automatically decode byte strings to strings
-# )
-#
-# redis_client_without_decoded_response = redis.StrictRedis(
-#     host=settings.REDIS_HOST,
-#     port=settings.REDIS_PORT,
-#     db=settings.REDIS_DB,
-#     decode_responses=False
-# )
+Environment = settings.ENVIRONMENT
 
-# Redis cluster client configuration
-redis_client = redis.RedisCluster(
-    startup_nodes=settings.REDIS_CLUSTER_NODES,
-    decode_responses=True  # Automatically decode byte strings to strings
-)
+if Environment == "development":
+    # # Redis client configuration
+    redis_client = redis.StrictRedis(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        db=settings.REDIS_DB,
+        decode_responses=True  # Automatically decode byte strings to strings
+    )
 
-redis_client_without_decoded_response = redis.RedisCluster(
-    startup_nodes=settings.REDIS_CLUSTER_NODES,
-    decode_responses=False
-)
+    redis_client_without_decoded_response = redis.StrictRedis(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        db=settings.REDIS_DB,
+        decode_responses=False
+    )
+    
+elif Environment == "production":
+    # Redis cluster client configuration
+    redis_client = redis.RedisCluster(
+        startup_nodes=settings.REDIS_CLUSTER_NODES,
+        decode_responses=True  # Automatically decode byte strings to strings
+    )
+
+    redis_client_without_decoded_response = redis.RedisCluster(
+        startup_nodes=settings.REDIS_CLUSTER_NODES,
+        decode_responses=False
+    )
